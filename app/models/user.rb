e700d90bad2825f6eaeaddada32d265dfc80d7b8
Coencidental <cdrexler1999@gmail.com>
2019-10-28 21:37:04 +1000

    Added rolify user role, and a callback method in User to assign it by default

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,13 @@
 class User < ApplicationRecord
+  rolify
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  after_create :assign_default_role
+
+  def assign_default_role
+    add_role(:user)
+  end
+  
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
