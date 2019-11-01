9b30af63d103253ce31c09d2916f38a2f98ba461
Coencidental <cdrexler1999@gmail.com>
2019-11-01 14:10:10 +1000

    Created a shared views folder, made an errors partial, write bucket model validations and tested them through the error partial

--- app/models/bucket.rb
+++ app/models/bucket.rb
@@ -1,4 +1,7 @@
 class Bucket < ApplicationRecord
+  validates :name, presence: true, length: {minimum: 2}
+  validates :description, presence: true, length: {minimum: 10}
+
   belongs_to :user
   has_many :items, dependent: :destroy
 end
