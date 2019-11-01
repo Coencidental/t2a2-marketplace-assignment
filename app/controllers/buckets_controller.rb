06b7f9f617914343d6bde53199dc54a312b6d6a9
Coencidental <cdrexler1999@gmail.com>
2019-11-01 11:58:02 +1000

    Created blank methods in buckets controller, checked models are passing through, constructed basic conditional view for user buckets and added routes for Buckets#User page

--- app/controllers/buckets_controller.rb
+++ app/controllers/buckets_controller.rb
@@ -1,8 +1,14 @@
 class BucketsController < ApplicationController
   def index
+    @buckets = Bucket.all.order(user_id: :desc)
   end
 
   def user
+    if current_user.buckets.count > 0
+      @buckets = current_user.buckets.all
+    else
+      @buckets = nil
+    end
   end
 
   def show
@@ -11,6 +17,24 @@ class BucketsController < ApplicationController
   def new
   end
 
+  def create
+
+  end
+
   def edit
+  end
+
+  def update 
+
+  end
+
+  def destroy 
+
+  end
+
+  private
+
+  def bucket_params
+    params.require(:bucket).permit(:name, :description)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,11 @@ Rails.application.routes.draw do
   
   root 'pages#index'
   get 'pages/about'
+  get 'buckets/user', to: 'buckets#user', as: 'user_buckets'
 
-  resources :buckets
+  resources :buckets do 
+  end
+
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
