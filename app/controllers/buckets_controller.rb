cfde98b70a2c26dbca0a2e8ec3b8c7743e0f6f73
Coencidental <cdrexler1999@gmail.com>
2019-11-01 11:36:07 +1000

    Generated buckets controller and basic views, updated routes with resources for buckets

---
+++ app/controllers/buckets_controller.rb
@@ -0,0 +1,16 @@
+class BucketsController < ApplicationController
+  def index
+  end
+
+  def user
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def edit
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  
   root 'pages#index'
   get 'pages/about'
+
+  resources :buckets
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
