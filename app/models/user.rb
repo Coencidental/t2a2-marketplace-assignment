546fe252a3ac6f962371f3079c4236a31e1ec4ac
Coencidental <cdrexler1999@gmail.com>
2019-11-01 10:38:59 +1000

    Created Bucket model for organising items, and added 'belongs to' in bucket model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   after_create :assign_default_role
+  has_many :buckets, dependent: :destroy
 
   def assign_default_role
     add_role(:user)
---
+++ db/migrate/20191101003757_create_buckets.rb
@@ -0,0 +1,11 @@
+class CreateBuckets < ActiveRecord::Migration[5.2]
+  def change
+    create_table :buckets do |t|
+      t.string :name
+      t.text :description
+      t.references :user, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
