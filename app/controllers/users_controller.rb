f8c9d39c48700ffa4b13fa2c05461ac75654eeaf
Coencidental <cdrexler1999@gmail.com>
2019-11-08 16:06:12 +1000

    Slight authorization modifications, heavy styling

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,5 +2,13 @@ class UsersController < ApplicationController
   
   def show
     @user = User.find_by(id: params[:id])
+    @sold = []
+    @user.buckets.each do |bucket|
+      bucket.items.each do |item|
+        if item.sold == true
+          @sold << item
+        end
+      end
+    end
   end
 end
